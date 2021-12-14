1a9f704252357aeda862312719b95578b1155021
elizabeth-oh <91884455+elizabeth-oh@users.noreply.github.com>
2021-12-14 18:28:04 +0100

    Imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-
+      <div className="container">
+      </div>
     </div>
   );
 }
